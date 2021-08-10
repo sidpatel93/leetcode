2fc7a2b9b4b95aab3464fea8942761895e25b055
Siddharth <sid.auburn@gmail.com>
2021-08-10 19:32:35 -0400

    solved number_dispeeared in appray problem

---
+++ find_number_disappeared_in_array.py
@@ -0,0 +1,27 @@
+
+
+def findDisappearedNumbers(nums):
+    arrayToReturn = []
+    compare = [0] * len(nums)
+    for i in nums:
+        compare[i-1] = 1
+    for j in range(len(compare)):
+        if (compare[j] == 0):
+            arrayToReturn.append(j+1)
+    return arrayToReturn
+
+
+print(findDisappearedNumbers([1,2,4,1,4,2]))
+
+
+
+
+test_cases = [
+    ([4,3,2,7,8,2,3,1], [5,6]),
+    ([1,1], [2])
+]
+
+def test_findDisappearedNumbers():
+    for test, expected in test_cases:
+        assert (findDisappearedNumbers(test) == expected)
+    
