0c9f254a1dce56c4e3d1d4ad0c5142521ac38330
Siddharth <sid.auburn@gmail.com>
2021-08-14 13:11:01 -0400

    convert-binary-to-decimal-in-linked-list

---
+++ convert-binary-singly-linked-list.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        
+        #Get the length of the linkedlist, since list is not empty(given)
+        length = 1
+        tail = head
+        while (tail.next):
+            tail = tail.next
+            length+=1
+            
+        # print(length)
+        
+        # traverse the list to find the element value and adjust the decimal value at each node 
+        
+        decimal = 0
+        tail = head
+        print(tail)
+        for i in reversed(range(length)):
+            # print(i)
+            decimal = decimal + ((tail.val)*(2**(i)))
+            # print(decimal)
+            tail = tail.next
+            # print(tail)
+        return decimal
